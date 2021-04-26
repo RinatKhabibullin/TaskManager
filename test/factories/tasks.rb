df699dc9db986030e8527edf7e906b89fb6b29f0
RinatKhabibullin <lmiksl@gmail.com>
2021-04-26 22:48:39 +0300

    refactoring

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
-    name { generate :string }
-    description { generate :string }
+    name
+    description
     author factory: :manager
     assignee factory: :developer
     traits_for_enum(:state, ['new_task', 'in_development', 'in_qa', 'in_code_review', 'ready_for_release', 'released', 'archived'])

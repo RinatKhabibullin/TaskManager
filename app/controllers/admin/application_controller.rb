fd2ec7a1f0044a51d08f5c1a2b9c919cf6ce5595
RinatKhabibullin <lmiksl@gmail.com>
2021-04-26 21:25:25 +0300

    add users list

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ApplicationController < ApplicationController
   include AuthHelper
   before_action :authenticate_user!, :authorize
+  helper_method :current_user
 
   def authorize
     render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false) if (forbidden?)
@@ -8,5 +9,10 @@ class Admin::ApplicationController < ApplicationController
 
   def forbidden?
     !current_user.is_a?(Admin)
+  end
+
+  def index
+    @q = User.ransack(params[:q])
+    @users = @q.result.order(:type).order(:id).page(params[:page]).per(10)
   end
 end

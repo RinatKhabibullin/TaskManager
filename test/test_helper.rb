b5a5c6aa3311388f5b889c79462d929f190b985c
RinatKhabibullin <lmiksl@gmail.com>
2021-05-25 23:12:12 +0300

    fix helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,20 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  if ENV['CI']
+    formatter SimpleCov::Formatter::SimpleFormatter
+  else
+    formatter SimpleCov::Formatter::MultiFormatter.new([
+      SimpleCov::Formatter::SimpleFormatter,
+      SimpleCov::Formatter::HTMLFormatter
+    ])
+  end
+end
+
 require 'coveralls'
 
 Coveralls.wear!

4b9fa77213e3f324d6862ac68562ed36223fddf8
RinatKhabibullin <lmiksl@gmail.com>
2021-05-25 20:49:25 +0300

    add Coveralls.wear to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,12 +3,8 @@ require_relative '../config/environment'
 require 'rails/test_help'
 require 'coveralls'
 
-if ENV['RAILS_ENV'] == 'test'
-  require 'simplecov'
-  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-  SimpleCov.start 'rails'
-  puts "required simplecov"
-end
+Coveralls.wear!
+
 class ActiveSupport::TestCase
   include AuthHelper
   include FactoryBot::Syntax::Methods

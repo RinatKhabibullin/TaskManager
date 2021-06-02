abfeed45d92e1d30b1c75d4dd73e5e3a38868930
RinatKhabibullin <lmiksl@gmail.com>
2021-06-02 21:59:12 +0300

    hide rollbar token

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,7 +2,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = '96fe24c28c6a4d2da8f1e08365620a78'
+  config.access_token = ENV['ROLLBAR_TOKEN']
 
   # Here we'll disable in 'test':
   if Rails.env.test?
